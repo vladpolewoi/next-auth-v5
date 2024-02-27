b6d04279460f42dd03f3325a1517efb6394ec48d
vlad polewoi <vlad.polewoi1@gmail.com>
2024-02-27 16:40:30 +0200

    feat: :sparkles: admin

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,33 +1,58 @@
 "use client"
 
+import { admin } from "@/actions/admin"
 import { RoleGate } from "@/components/auth/role-gate"
+import { FormSuccess } from "@/components/form-success"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { useCurrentRole } from "@/hooks/use-current-role"
 import { UserRole } from "@prisma/client"
+import { toast } from "sonner"
 
 const AdminPage = () => {
 	const role = useCurrentRole()
 
+	const onApiRouteClick = () => {
+		fetch("/api/admin").then((res) => {
+			if (res.ok) {
+				toast.success("Allowed API route")
+			} else {
+				toast.error("Forbidden API route")
+			}
+		})
+	}
+
+	const onServerActionClick = () => {
+		admin().then((data) => {
+			if (data.error) {
+				toast.error(data.error)
+			}
+
+			if (data.success) {
+				toast.success(data.success)
+			}
+		})
+	}
+
 	return (
 		<Card className="w-[600px]">
 			<CardHeader>
-				<p className="text-2xl font-semibold text-center">
-					You are an admin. You have access to this page because you are
-					authenticated and your role is &quot;admin&quot;.
-				</p>
+				<p className="text-2xl font-semibold text-center">Admin</p>
 			</CardHeader>
+
 			<CardContent className="space-y-4">
 				<RoleGate allowedRole={UserRole.ADMIN}>
-					<p className="text-lg">
-						Your role is &quot;{role}&quot;. You are allowed to access this
-						page.
-					</p>
+					<FormSuccess message="You are an admin" />
 				</RoleGate>
 
 				<div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
 					<p className="text-sm font-medium">Admin-only API Route</p>
-					<Button>Click to test</Button>
+					<Button onClick={onApiRouteClick}>Click to test</Button>
+				</div>
+
+				<div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+					<p className="text-sm font-medium">Admin-only Server Action</p>
+					<Button onClick={onServerActionClick}>Click to test</Button>
 				</div>
 			</CardContent>
 		</Card>
